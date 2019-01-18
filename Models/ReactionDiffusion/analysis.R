2afdff88478c927293c526843ef164d3e7040fea
Juste Raimbault <juste.raimbault@polytechnique.edu>
2019-01-18 14:31:55 +0100

    parcomp_lyapounovs

--- Models/ReactionDiffusion/analysis.R
+++ Models/ReactionDiffusion/analysis.R
@@ -6,14 +6,14 @@ library(ggplot2)
 library(reshape2)
 library(segmented)
 
-source(paste0(Sys.getenv('CS_HOME'),'/Organisation/Models/Utils/R/plots.R'))
+#source(paste0(Sys.getenv('CS_HOME'),'/Organisation/Models/Utils/R/plots.R'))
 
 ###
-# liapounov configurations
+## 1) Liapounov configurations
 
 experiment='20190112_1620_LOCAL'
 
-res<-as.tbl(read.csv('exploration/',experiment,'.csv'))
+res<-as.tbl(read.csv(paste0('exploration/',experiment,'.csv')))
 
 resdir=paste0(Sys.getenv('CS_HOME'),'/SpatialComplexity/Results/ReactionDiffusion/',experiment,'/');dir.create(resdir)
 
@@ -22,34 +22,35 @@ resdir=paste0(Sys.getenv('CS_HOME'),'/SpatialComplexity/Results/ReactionDiffusio
 res$id=paste0(res$growthrate,res$population,res$alphalocalization,res$diffusion,res$diffusionsteps)
 res$id=as.numeric(as.factor(res$id))
 sres=res%>%group_by(id)%>%summarize(count=n())
-sres[sres$count>500,]
+#sres[sres$count>500,]
+#sres[sres$count==1000,]
 
 tres = melt(data=res,id.vars=c("growthrate","population","replication","alphalocalization","diffusion","diffusionsteps","id"))
 tres$t=as.numeric(substring(as.character(tres$variable),first=10))
 
 #id=643,694,686,1022,801
-id=801
-d=tres[tres$id==id&tres$value>0,]
+#id=801
+#d=tres[tres$id==id&tres$value>0,]
 
 #tstep=2
-tstep=5
-g=ggplot(d[d$t%%tstep==0,],aes(x=t,y=log(value),group=t))
+#tstep=5
+#g=ggplot(d[d$t%%tstep==0,],aes(x=t,y=log(value),group=t))
 #g+geom_point(pch='.')+stat_smooth(method = 'gam')
-g+geom_boxplot()+xlab("time")+ylab("log(d)")+
-  ggtitle(bquote(N[G]/P[tot]*"="*.(round(d$growthrate[1]/d$population[1],digits=5))*" ; "*alpha*"="*.(round(d$alphalocalization[1],digits=2))*" ; "*beta*"="*.(round(d$diffusion[1],digits=2))*" ; "*n[d]*"="*.(round(d$diffusionsteps[1]))))+stdtheme
-ggsave(file=paste0(resdir,'configdist_boxplot_id',id,'.png'),width=20,height = 15,units='cm')
+#g+geom_boxplot()+xlab("time")+ylab("log(d)")+
+#  ggtitle(bquote(N[G]/P[tot]*"="*.(round(d$growthrate[1]/d$population[1],digits=5))*" ; "*alpha*"="*.(round(d$alphalocalization[1],digits=2))*" ; "*beta*"="*.(round(d$diffusion[1],digits=2))*" ; "*n[d]*"="*.(round(d$diffusionsteps[1]))))+stdtheme
+#ggsave(file=paste0(resdir,'configdist_boxplot_id',id,'.png'),width=20,height = 15,units='cm')
 
 
 # test piecewise linear fitting
-seg<-segmented(lm(data=d,log(value)~t))
-slope(seg)$t[1,1]
-summary(seg)$adj.r.squared
+#seg<-segmented(lm(data=d,log(value)~t))
+#slope(seg)$t[1,1]
+#summary(seg)$adj.r.squared
 
-adjr2=c();lambda1=c();lambda2=c();ids=sres$id[sres$count>500];breaks=c()
+adjr2=c();lambda1=c();lambda2=c();ids=sres$id[sres$count==1000];breaks=c()
 #for(id in ids){
   # this should be parallelized when will have more runs
 library(doParallel)
-cl <- makeCluster(20,outfile='loggwr')
+cl <- makeCluster(50)
 registerDoParallel(cl)
 
 res <- foreach(i=1:length(ids)) %dopar% {
@@ -64,8 +65,10 @@ res <- foreach(i=1:length(ids)) %dopar% {
   return(c(lambda1=slope(seg)$t[1,1],lambda2=slope(seg)$t[2,1],breaks=seg$psi[2],adjr2=summary(seg)$adj.r.squared))
 }
 
+stopCluster(cl)
+
 #save(adjr2,lambda1,lambda2,ids,breaks,file=paste0(resdir,'lyapounov.RData'))
-save(res,file=paste0(resdir,'lyapounov.RData'))
+save(res,file=paste0(resdir,'lyapounov_',experiment,'.RData'))
 
 #summary(lambda1)
 #summary(lambda2)
@@ -81,5 +84,13 @@ save(res,file=paste0(resdir,'lyapounov.RData'))
 
 #summary(lm(data=dd,lambda1~alpha+beta+nd+growthrate+population+relgrowthrate))
 #summary(lm(data=dd,lambda2~alpha+beta+nd+growthrate+population+relgrowthrate))
+
+
+###
+## 2) Morpho trajectories
+
+
+
+
 
 
